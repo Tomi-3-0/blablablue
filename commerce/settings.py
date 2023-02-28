979d62d0a772be28fd1b0a13fde13755917d5cfb
Caleb Omoniyi <81532297+Tomi-3-0@users.noreply.github.com>
2023-02-28 22:15:03 +0100

    updated allowed hosts

--- commerce/settings.py
+++ commerce/settings.py
@@ -35,7 +35,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&
 
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
-ALLOWED_HOSTS = ['https://web-production-d978.up.railway.app/','127.0.0.1']
+ALLOWED_HOSTS = ['tobi-commerce.herokuapp.com','127.0.0.1']
 
 # During development, you can instead set just the base URL
 # (you might decide to change the site a few times).
